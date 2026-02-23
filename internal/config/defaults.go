ca4b1bb54754a85922841d2fb2e022c14a45c2c4
saba <tameemtakouan@hotmail.com>
2026-02-23 17:04:42 +0800

    fix(pac): include bilibili/wechat rules in defaults

--- internal/config/defaults.go
+++ internal/config/defaults.go
@@ -5,9 +5,10 @@ package config
 // The list is intentionally small and CDN-friendly, and should cover both IPv4 and IPv6 CN traffic.
 func DefaultPACRuleURLs() []string {
 	return []string{
-		"https://gh-proxy.org/https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/China/China.list",
+		"https://gh-proxy.org/https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/BiliBili/BiliBili.list",
+		"https://gh-proxy.org/https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/WeChat/WeChat.list",
+		"https://gh-proxy.org/https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/ChinaMaxNoIP/ChinaMaxNoIP.list",
 		"https://gh-proxy.org/https://raw.githubusercontent.com/fernvenue/chn-cidr-list/master/ipv4.yaml",
 		"https://gh-proxy.org/https://raw.githubusercontent.com/fernvenue/chn-cidr-list/master/ipv6.yaml",
 	}
 }
-
